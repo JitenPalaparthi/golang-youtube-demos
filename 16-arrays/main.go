68b0dada84e9b33fe7ccbb3cdb52d6546c02f6e2
Jiten <jpalaparthi@gmail.com>
2025-01-20 08:39:00 +0530

    add tasks directory

---
+++ 16-arrays/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	var arr1 [15]int
+
+	for i := 0; i < len(arr1); i++ {
+		arr1[i] = rand.Intn(100)
+	}
+
+	fmt.Println(arr1)
+
+	var arr2d [2][2]int
+
+	for i1, a1 := range arr2d {
+		for i2 := range a1 {
+			arr2d[i1][i2] = rand.Intn(100)
+		}
+	}
+
+	//fmt.Println(arr2d)
+
+	for _, a1 := range arr2d {
+		for _, v := range a1 {
+			print(v, " ")
+		}
+		println()
+	}
+
+	arr2d1 := [2][2]int{{1, 2}, {3, 4}}
+	fmt.Println(arr2d1)
+
+	arr3d := [2][2][2]int{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}
+	println("print 3darr")
+
+	for _, a1 := range arr3d {
+		for _, a2 := range a1 {
+			for _, v := range a2 {
+				print(v, " ")
+			}
+		}
+	}
+	println()
+}
---
+++ tasks/arrays.md
@@ -0,0 +1,20 @@
+1. Take a 2d array 
+[1 2]
+[3 4]
+
+Do the array transpose 
+
+2. Take same 2d array perform addition of rows and keep it in another array [2][3]int
+
+[1 2]  --> 1 2 3 
+[3 4]  --> 3 4 7
+
+3. Take same 2d array and perform addition of columns and keep it in another array [3][2]int 
+
+[1 2]
+[3 4] 
+
+output 
+[1 2]
+[3 4]
+[4 6]
